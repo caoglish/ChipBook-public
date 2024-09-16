8df79d308b89fbbc0d252f3eb6de638417b27215
Caoglish <caoglish@sina.com>
2024-09-16 23:15:15 +1000

    add logic if isAutenticated show all menu otherwise show home only.

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,16 @@
   <v-app>
     <v-main>
       <nav>
-        <router-link to="/">Home</router-link>|
-        <router-link to="/game">Game</router-link>|
-        <router-link to="/history">History</router-link>|
-        <router-link to="/player">Player</router-link>|
-        <router-link to="/about">About</router-link>
-|
+        <!-- 显示 Home 链接，始终可见 -->
+        <router-link to="/">Home</router-link> |
+        
+        <!-- 仅在用户已登录时显示以下链接 -->
+        <template v-if="isAuthenticated">
+          <router-link to="/game">Game</router-link> |
+          <router-link to="/history">History</router-link> |
+          <router-link to="/player">Player</router-link> |
+          <router-link to="/about">About</router-link>
+        </template>
       </nav>
       <router-view />
     </v-main>
@@ -15,18 +19,26 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, computed,onMounted } from "vue";
+import { useLoginUserStore } from "@/store/LoginUserStore";// 导入 Pinia store
 
 export default defineComponent({
   name: "App",
 
-  data() {
+  setup() {
+    const loginUserStore = useLoginUserStore(); // 获取登录用户 store
+
+    // 计算属性，检查用户是否已登录
+    const isAuthenticated = computed(() => !!loginUserStore.user);
+
+	onMounted(() => {
+      loginUserStore.checkAuthState(); // 检查用户登录状态
+    });
+
     return {
-      //
+      isAuthenticated,
+	  //loginUserStore
     };
   },
 });
 </script>
-
-
-
