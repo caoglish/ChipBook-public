8c4a932ef8a24aef36a41860849b5f68c7d558c2
Caoglish <caoglish@sina.com>
2025-02-12 02:35:58 +1100

    VNumberInput need to manual add to dependency list

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,11 +1,13 @@
 // Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
-
+import { VNumberInput } from 'vuetify/labs/VNumberInput'
 // Vuetify
 import { createVuetify} from 'vuetify'
 
-export default createVuetify(
- 
+export default createVuetify({
+	components: {
+		VNumberInput,
+	  },
   // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+})
