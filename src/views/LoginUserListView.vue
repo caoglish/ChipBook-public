867e714ba388b609534f1f5b7e8a2ba5da8482d5
Caoglish <caoglish@sina.com>
2025-03-05 23:43:55 +1100

    login user loading 加入效果

--- src/views/LoginUserListView.vue
+++ src/views/LoginUserListView.vue
@@ -1,31 +1,31 @@
 <template>
 	<v-container>
 	  <h2 class="mb-4">Login Users</h2>
-	  <v-btn color="primary" @click="fetchUsers">刷新用户列表</v-btn>
+	  <v-btn color="primary" @click="refreshUsers"  class="mb-6">刷新用户列表</v-btn>
+	  
 	  
 	  <v-data-table 
-      v-if="store.users.length"
+       v-if="!loading && store.users.length"
       :headers="headers"
       :items="store.users"
       item-value="uid"
       class="elevation-1"
 	  :items-per-page="100"
-    >
-    
-    </v-data-table>
+    ></v-data-table>
   
-	  <p v-else class="text-muted">暂无用户数据</p>
+	<p v-else-if="!loading" class="text-muted">暂无用户数据</p>
+	<v-progress-linear v-if="loading" indeterminate :height="12" ></v-progress-linear>
 	</v-container>
   </template>
   
   <script setup>
-  import { onMounted,computed } from 'vue'
+  import { onMounted,computed ,ref} from 'vue'
   import { useLoginUserCollectionStore } from '@/store/useLoginUserCollectionStore';
 
   
   const store = useLoginUserCollectionStore()
   const {  fetchUsers } = store
-
+  const loading = ref(false)
 
   const headers = computed(() => [
   { title: 'UID', key: 'uid' },
@@ -35,7 +35,14 @@
   { title: '最后登录', key: 'lastLogin' }
 ]);
   
-  onMounted(fetchUsers)
+
+const refreshUsers = async () => {
+  loading.value = true;
+  await fetchUsers();
+  loading.value = false;
+}
+
+  onMounted(refreshUsers)
   
 
   </script>
