0479ffaca3736310240dd386d86d17b72136bd8e
Caoglish <caoglish@sina.com>
2025-03-06 01:04:05 +1100

    remaining dialog change     - 单击确定，确定disable并显示loading状态     - 防止重复点击     - 打开对话框，保证不是loading状态

--- src/components/gameManagement/RemainingChipsDialog.vue
+++ src/components/gameManagement/RemainingChipsDialog.vue
@@ -6,7 +6,6 @@
 				<v-chip color="primary" variant="flat">{{ gameStore.currentPlayer.player_display_name }}</v-chip>
 			</v-card-title>
 			<v-card-text>
-				
 				<v-number-input 
 				v-model="gameStore.remainingAmount" 
 				label="剩余筹码数" 
@@ -16,7 +15,11 @@
 				></v-number-input>
 			</v-card-text>
 			<v-card-actions>
-				<v-btn color="blue darken-1" @click="confirmRemaining" variant="flat">确认</v-btn>
+				
+				<v-btn color="blue darken-1" @click="confirmRemaining" variant="flat" :disabled="isProcessing">
+					<v-progress-circular v-if="isProcessing" indeterminate size="20" class="mr-2"></v-progress-circular>
+					确认
+				</v-btn>
 				<v-btn color="grey darken-1" @click="gameStore.cancelRemaining" variant="outlined">取消</v-btn>
 			</v-card-actions>
 		</v-card>
@@ -25,7 +28,20 @@
 
 <script setup>
 import { useGameStore } from '@/store/useGameStore';
-import { debounce } from '@/Lib/Helper';
+import { ref, watch } from 'vue';
+const isProcessing = ref(false);
+
 const gameStore = useGameStore();
-const confirmRemaining = debounce(gameStore.confirmRemaining);
+const confirmRemaining = async () => {
+	if (isProcessing.value) return; // 防止重复点击
+	isProcessing.value = true;
+	await gameStore.confirmRemaining();
+	isProcessing.value = false;
+};
+
+watch(() => gameStore.remainingDialog, (newVal) => {
+	if (newVal) {
+		isProcessing.value = false;
+	}
+});
 </script>
